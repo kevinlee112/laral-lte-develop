7c725dbedbcf7b2708f46226451ab151379005b2
liqingyuan <2298829547@qq.com>
2018-07-24 12:16:36 +0800

    request admin

---
+++ app/Http/Requests/Admin/PermissionCreateRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use App\Http\Requests\Request;
+
+class PermissionCreateRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'=>'required|unique:admin_permissions|max:255',
+            'label'=>'required|max:255',
+            'cid'=>'required|int',
+            'description'=>'required:max:255'
+        ];
+    }
+}
